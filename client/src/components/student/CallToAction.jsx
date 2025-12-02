3ab1e73e3c8e6793eb9dfd123c6d12247a8f889c
rushi-0369 <23211a0556@bvrit.ac.in>
2025-12-02 15:14:49 +0530

    Get started button

--- client/src/components/student/CallToAction.jsx
+++ client/src/components/student/CallToAction.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
 import {assets} from '../../assets/assets'
+import { useClerk } from '@clerk/clerk-react'
 const CallToAction = () => {
+  const {openSignIn} = useClerk()
   return (
     <div className='flex flex-col items-center gap-4 pt-10 pb-24 px-8 md:px-0'>
       <h1 className='text-xl md:text-4xl text-gray-800 font-semibold'>Learn anything, anytime, anywhere</h1>
       <p className='text-gray-500 sm:text-sm'>Discover high-quality courses designed to help you learn, grow, and achieve your goals at your own pace.</p>
       <div className='flex items-center font-medium gap-6 mt-4'>
-        <button className='px-10 py-3 rounded-md text-white bg-blue-600'>Get started</button>
+        <button onClick={()=>openSignIn()} className='px-10 py-3 rounded-md text-white bg-blue-600'>Get started</button>
         <button className='flex items-center gap-2'>Learn more <img src={assets.arrow_icon} alt="arrow_icon" /></button>
       </div>
     </div>
