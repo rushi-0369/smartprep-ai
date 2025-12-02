a3664b927be17eaba1ba5989585b68b40ffb1e36
rushi-0369 <23211a0556@bvrit.ac.in>
2025-12-02 11:54:52 +0530

    Adding css to the couseCard and courseSection

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -14,6 +14,10 @@ export default {
         'home-heading-large': ['48px', '56px'],
         'default': ['15px', '21px'],
       },
+      gridTemplateColumns:{
+        'auto': 'repeat(auto-fit, minmax(200px, 1fr))'
+        
+      }
     },
   },
   plugins: [],
