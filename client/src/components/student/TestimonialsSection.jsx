65227bea4923c9eadd12c6e163f0614bda421ee7
rushi-0369 <23211a0556@bvrit.ac.in>
2025-12-02 13:12:24 +0530

    Removed anchor read more(not useful)

--- client/src/components/student/TestimonialsSection.jsx
+++ client/src/components/student/TestimonialsSection.jsx
@@ -47,7 +47,6 @@ const TestimonialsSection = () => {
               </div>
               <p className="text-gray-500 mt-5">{testimonial.feedback}</p>
             </div>
-            <a href="#" className='text-blue-500 underline px-5'>Read more</a>
           </div>
         ))}
       </div>
