87c61abbd91a56bca0df5b43258882361e97ae72
rushi-0369 <23211a0556@bvrit.ac.in>
2025-12-04 14:28:50 +0530

    Created CourseDetails page

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { dummyCourses } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
+import humanizeDuration from "humanize-duration";
 
 export const AppContext = createContext();
 
@@ -14,16 +15,46 @@ export const AppContextProvider = ({ children }) => {
     setAllCourses(dummyCourses);
   };
 
-  const calculateRating = (course)=>{
-    if(!course.courseRatings || course.courseRatings.length === 0){
+  const calculateRating = (course) => {
+    if (!course.courseRatings || course.courseRatings.length === 0) {
       return 0;
     }
-    let totalRating = 0
-    course.courseRatings.forEach(rating => {
-      totalRating += rating.rating
-    })
-    return totalRating/course.courseRatings.length;
-  }
+    let totalRating = 0;
+    course.courseRatings.forEach((rating) => {
+      totalRating += rating.rating;
+    });
+    return totalRating / course.courseRatings.length;
+  };
+
+  const calculateChapterTime = (chapter) => {
+    let time = 0;
+    chapter.chapterContent.forEach((lecture) => {
+      time += Number(lecture.lectureDuration) || 0;
+    });
+    if (time === 0) return "0m";
+    return humanizeDuration(time * 60 * 1000, { units: ["h", "m"] });
+  };
+
+  const calculateCourseDuration = (course) => {
+    let time = 0;
+    course.courseContent.forEach((chapter) => {
+      chapter.chapterContent.forEach((lecture) => {
+        time += Number(lecture.lectureDuration) || 0;
+      });
+    });
+    if (time === 0) return "0m";
+    return humanizeDuration(time * 60 * 1000, { units: ["h", "m"] });
+  };
+
+  const calculateNoOfLectures = (course) => {
+    let totalLectures = 0;
+    course.courseContent.forEach((chapter) => {
+      if (Array.isArray(chapter.chapterContent)) {
+        totalLectures += chapter.chapterContent.length;
+      }
+    });
+    return totalLectures;
+  };
 
   useEffect(() => {
     fetchAllCourses();
@@ -35,12 +66,11 @@ export const AppContextProvider = ({ children }) => {
     navigate,
     calculateRating,
     isEducator,
-    setIsEducator
+    setIsEducator,
+    calculateChapterTime,
+    calculateCourseDuration,
+    calculateNoOfLectures,
   };
 
-  return (
-    <AppContext.Provider value={value}>
-      {children}
-    </AppContext.Provider>
-  );
+  return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
